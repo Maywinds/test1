539adacfea6b49e273aa5267521300c94c444cad
HWJ <HWJ_MF@163.com>
2018-12-07 18:19:55 +0800

    add test the second time

--- test1/src/test/java/name/hwj/test/test1.java
+++ test1/src/test/java/name/hwj/test/test1.java
@@ -4,4 +4,9 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("nihao");
     }
+    public int method1(){
+        int i=0;
+        i+=10;
+        return i;
+    }
 }
