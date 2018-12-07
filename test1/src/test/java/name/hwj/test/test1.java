ed5b9f39c8c22c37fcd5ce98a1b6f4a731635a39
HWJ <HWJ_MF@163.com>
2018-12-07 18:44:46 +0800

    modified some content

--- test1/src/test/java/name/hwj/test/test1.java
+++ test1/src/test/java/name/hwj/test/test1.java
@@ -3,6 +3,7 @@ package name.hwj.test;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("nihao");
+        System.out.println("sorry i want to add something!");
     }
     public int method1(){
         int i=0;
