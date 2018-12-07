044d2ba93b40cbcc6cfe06f1c76ffcca8ea26066
HWJ <HWJ_MF@163.com>
2018-12-07 18:57:03 +0800

    add travel to repo the first time

--- test1/src/test/java/name/hwj/test/test1.java
+++ test1/src/test/java/name/hwj/test/test1.java
@@ -4,6 +4,8 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("sorry i want to add something!");
+        System.out.println("add something on hwj branch");
+        System.out.println("nihao master");
     }
     public int method1(){
         int i=0;
